92d82d8841a71d316e240bba03b9482f11a91e88
alexrichard1994 <alexandrorichard@hotmail.com>
2020-11-30 14:39:58 -0500

    commit

--- js/funciones.js
+++ js/funciones.js
@@ -1,22 +1,49 @@
-
-
-function abrirPanel(agregar){
+function abrirPanel(panelInicial){
     const paneles = document.querySelectorAll(".panel")
     paneles.forEach(panel=>panel.classList.remove("activa"));
 
     const opciones = document.querySelectorAll(".opcion")
-    opciones.forEach(opcion=>opcion.classList.remove("activo"));
+    opciones.forEach(opcion=>opcion.classList.remove("activo"))
 
-    document.getElementById(agregar).classList.add("activa")
+    document.getElementById(panelInicial).classList.add("activa")
 }
 
 
-function abrirPanel(listar){
-    const paneles = document.querySelectorAll(".panel")
-    paneles.forEach(panel=>panel.classList.remove("activa"));
 
-    const opciones = document.querySelectorAll(".opcion")
-    opciones.forEach(opcion=>opcion.classList.remove("activo"));
+//crear funcion que recibe como parametro el 
+//obj obtenido con fetch()que almacene el 
+//contenido en una variable, utilizar forEach() 
+fetch('../data/bd.json')
+    .then(response=>response.json())
+    .then(datos=>agregarProyectos(datos))
 
-    document.getElementById(listar).classList.add("activa")
+    function agregarProyectos(datos){
+        const proyectos=datos
+        const tabla=document.querySelector("#listar")
+
+        proyectos.forEach(proyecto=>{
+            tabla.innerHTML+=`
+                ${proyecto.nombre}
+                ${proyecto.lenguage}
+                ${proyecto.monto}
+        `
+
+    })
+}
+
+    function insertar(evt){
+          evt.preventDefault()
+          
+          const valores=[{
+                nombre:document.getElementById('nombre').value,
+                monto:document.getElementById('monto').value,
+          }]
+          agregarProyectos(valores)
+          limpiarFormulario()
+}
+     
+    function limpiarFormulario(){
+           document.getElementById("nombre").value="";
+           document.getElementById("lenguage").value="";
+           document.getElementById("monto").value="";
 }
